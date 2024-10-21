226a543f65e74890061a924c17ea86985957ff30
unknown <abdulbatinchy@gmail.com>
2024-10-21 07:48:41 +0600

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>hellow</h1>
+      <Button className="ml-10 bg-red-400 text-blue-400">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
